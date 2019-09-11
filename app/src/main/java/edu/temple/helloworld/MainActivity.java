b0d4c52ba5d7e055ea459ac188a86f306d4ef194
Viet Pham <tug36669@temple.edu>
2019-09-11 10:05:19 -0400

    Added a button. Anchored it horizontally and vertically to the textView and the left border.
    Added OnClickListener and textView.setText()

--- app/src/main/java/edu/temple/helloworld/MainActivity.java
+++ app/src/main/java/edu/temple/helloworld/MainActivity.java
@@ -3,12 +3,24 @@ package edu.temple.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView myText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.clickButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                myText = findViewById(R.id.textView);
+                myText.setText("You have clicked the button");
+            }
+        });
+
     }
 }
